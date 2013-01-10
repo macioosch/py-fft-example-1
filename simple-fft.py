8eee99172743d524fe7d55f2bcfb854e78b7399e
Maciej Chudak <macioosch@gmail.com>
2013-01-10 21:40:00 +0100

    style change

--- simple-fft.py
+++ simple-fft.py
@@ -1,32 +1,36 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from pylab import *
-from numpy import *
-from scipy import *
+#from pylab import *
+#from numpy import *
+#from scipy import *
+import matplotlib.pyplot as plt
+import numpy as np
 
 w = 100           # częstotliwość próbkowania [Hz]
 T = 1            # rozważany okres [s]
 
 n = T * w        # liczba próbek
-t = linspace(0, T, n, endpoint=False) # punkty na osi OX [s]
+t = np.linspace(0, T, n, endpoint=False) # punkty na osi OX [s]
 
 amplituda = 2
-czestotliwosc = 50
-f = lambda t : amplituda *sin( t *2*pi *czestotliwosc)    # def. funkcji
+czestotliwosc = 10
+f = lambda t : amplituda *np.sin( t *2*np.pi *czestotliwosc)    # def. funkcji
 signal = f(t)                 # funkcja spróbkowana
 
-subplot(211)
-plot(t, signal, '*')
-xlabel("czas [s]")
-ylabel("sygnał [j.u.]")
+fig = plt.figure()
+ax1 = fig.add_subplot(211)
+ax1.plot(t, signal, '*')
+plt.xlabel("czas [s]")
+plt.ylabel("sygnał [j.u.]")
 
-signal1 = fft(signal)      
-signal1 = abs(signal1) /( amplituda *T*w/2)        # moduł 
+signal1 = np.fft.fft(signal)      
+signal1 = abs(signal1)        # moduł 
 
-subplot(212)
-freqs = linspace(0, w, n, endpoint=False)
-stem(freqs, signal1, '-*')
-xlabel("częstotliwość [Hz]")
-ylabel("Intensywność składowej [j.u.]")
+ax2 = fig.add_subplot(212)
+#freqs = np.linspace(0, w, n, endpoint=False)
+freqs = np.fft.fftfreq( signal.size, 1/w)
+ax2.stem(freqs, signal1, '-*')
+plt.xlabel("częstotliwość [Hz]")
+plt.ylabel("Intensywność składowej [j.u.]")
 
-show()
+plt.show()
